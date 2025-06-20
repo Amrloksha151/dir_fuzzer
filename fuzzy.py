a03efd9b83dcfb8875ed8df06c4bf57e4bdff731
Amr Loksha <amrloksha151@proton.me>
2025-06-20 23:23:52 +0300

    I have to fix that shit

--- fuzzy.py
+++ fuzzy.py
@@ -5,6 +5,12 @@ wordlist = input("Wordlist: ")
 
 with open(wordlist, "r") as file:
     for line in file:
-        response = requests.get(target + line)
-        if not (response.status_code == 404):
-            print(f"{line}:{response.status_code}")
+        try:
+            if "#" in line:
+                continue
+            url = f"{target}/{line.replace("\n", "")}"
+            response = requests.get(url)
+            if not ("Oops... ! the page you looking for isnt there!" in response.text):
+                print(f"{line.replace("\n", "")}:{response.status_code}")
+        except KeyboardInterrupt:
+            break # solve this serious problem
